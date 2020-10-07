62c480aa899745440135064a3d0e19bb5a2cf9bd
Steven Chase <schase15@gmail.com>
2020-10-07 11:25:22 -0400

    Combined Model Selection page with Model Scores

--- pages/evaluation_scores.py
+++ pages/evaluation_scores.py
@@ -11,12 +11,16 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
+            ## Model Selection
+
+            Having defined my target and evaluation metrics, I then set out to explore different types of models to determine which would return the highest evaluation metrics. As this is a classification problem, I chose to explore Random Forest, XGBoost and Support Vector Machine. Additionally, I explored two linear classification models, Logistic Regression and Linear Support Vector Machine. Through experimentation I determined that Random Forest gave me the best results.
+        
             ## Model scores:
 
             After cleaning my data and selecting my features *(for more on feature selection please see the “Feature Selection” page)* I split my data into training, validation and test datasets. I started by running a baseline model to determine the null accuracy and f1 score. That is, the evaluation scores if we were to guess that every patient had a classification of minor risk of mortality (the most frequent target class). 
 
 
-            With a baseline score to beat, I trained several different types of models to see which type resulted in the best scores. The different models and scores are posted to the right. 
+            With a baseline score to beat, I trained the above mentioned models to see which resulted in the best evaluation scores. The different models and their respective scores are posted to the right. 
             
             #### To see the raw code including hyperparameter tuning, please visit my GitHub repository linked below. 
 
