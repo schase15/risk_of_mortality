a5b78f86c840be645d70edcd5c7862a0a94e3e08
Steven Chase <schase15@gmail.com>
2020-10-12 13:21:21 -0400

    Clarified Shapley and PDP plot explanations

--- pages/insights.py
+++ pages/insights.py
@@ -17,7 +17,7 @@ column1 = dbc.Col(
         
             ## Insights
 
-            Besides simply the predictive capabilities of the model, we can also draw further insights by looking at how the features interact with the model and with each other. To begin with let’s look at the permutation importances for the final model.
+            Besides the predictive capabilities of the model, we can also draw further insights by looking at how the features interact with the model and with each other. To begin with let’s look at the permutation importances for the final model.
 
             """
         ),
@@ -28,7 +28,7 @@ column1 = dbc.Col(
 
         dcc.Markdown(
             """
-            As mentioned before, this shows us the weight of effect that each feature has on the model’s predictions. This shows us that age group has the largest impact on categorizing the risk of mortality but tells us nothing of how it effects it. The partial dependence plot below shows us exactly how belonging to each age group effects the probability of being categorized for each risk class. 
+            As mentioned before, this shows us the weight of effect that each feature has on the model’s predictions. This shows us that 'age group' has the largest impact on categorizing the risk of mortality but tells us nothing of how it effects it. The partial dependence plot below shows us exactly how belonging to each age group effects the probability of being categorized for each risk class. 
 
             """
         ),
@@ -39,9 +39,9 @@ column1 = dbc.Col(
 
         dcc.Markdown(
             """
-            Let’s examine the first subplot for minor risk (upper left). The y-value represents the effect the age group has on the probability of being assigned a minor risk of mortality. We can see, as age increases the probability that you will be assigned a risk category of ‘minor’ decreases. This is reflected in the other three plots. They represent moderate, major and extreme risk of mortality. For each of them, probability of being assigned that risk class increases with age. 
+            Let’s examine the first subplot for 'minor risk' (upper left). The y-value represents the effect the age group has on the probability of being assigned a minor risk of mortality. We can see, as age increases the probability that you will be assigned a risk category of ‘minor’ decreases. This is reflected in the other three plots. They represent 'moderate', 'major' and 'extreme' risk of mortality. For each of them, probability of being assigned that risk class increases with age. 
 
-            Again, the below graph is indicating how features are interacting with the model and its predictions. However, this time it shows how two features are interacting. The graph below looks at age group and whether the admittance was medical or surgical, and returns probabilities of being designated minor, moderate, major or extreme risk of mortality. 
+            Again, the below graph is indicating how features are interacting with the model and its predictions. However, this time it shows how two features are interacting to influence the mortality risk prediciton. The graph below looks at age group and whether the admittance was medical or surgical, and returns probabilities of being designated 'minor', 'moderate', 'major' or 'extreme' risk of mortality. 
 
             """
         ),
@@ -52,9 +52,9 @@ column1 = dbc.Col(
 
         dcc.Markdown(
             """
-            Looking at the plot for minor risk of mortality (upper left), we see that if the patient is young (0 to 17) and there for a medical procedure rather than surgery, they have an 80% chance of being classified as having a minor risk of mortality. Those who are in the oldest age group (70 or older), only have a 25% chance of being categorized as a minor risk. The older age group has a higher probability of being categorized as moderate (35%) or major (31%). 
-
-            Finally, we can look at how each feature effected our final prediction for an individual in our test data. 
+            Now let's examine the plot for 'minor risk' of mortality (upper left). We can see that if the patient is young (0 to 17) and admitted to the hospital for a medical procedure rather than surgery, they have an 80% chance of being classified as having a 'minor risk' of mortality. If they are admitted to the hospital for surgery, then the probability of being predicted as a 'minor risk' of mortality drops to 64% while it increases for the higher risk levels. Those who are in the oldest age group (70 or older) and are at the hospital for medical admittance only have a 25% chance of being categorized as a 'minor risk'. 
+            From these partial dependence plots we can see that the risk increases both with age, as well as with a surgical admittance versus a medical admittance.  
+            Finally, we can look at one individual in our test data to see how each feature effected our final prediction.
 
             **Take a random patient from our test data:**
             """
@@ -66,7 +66,7 @@ column1 = dbc.Col(
 
         dcc.Markdown(
             """
-            Using Shapley values, we can look at how the model predicted the probability of each class, and then selected the class with the highest probability as its prediction. The red arrows are the features that push the probability higher, and the blue classes push the probability lower. The length of the colored bar is the amount of impact the feature had on the probability. 
+            Using Shapley values, we can look at how the model predicted the probability of each class, and then selected the class with the highest probability as its prediction. The red arrows are the features that push the probability for that class higher, and the blue arrows push the probability lower. The length of the colored bar is the amount of impact the feature had on influencing the prediction. 
 
             __Shapley values for Minor Risk of Mortality:__
             """
@@ -108,7 +108,8 @@ column1 = dbc.Col(
 
         dcc.Markdown(
             """
-            In this case class with the highest probability (0.72) was the minor risk of mortality. Therefore, the model predicted that this specific patient would have a minor risk of mortality. This was also the case when we look at the actual records, so in this instance the model predicted correctly. 
+            For this individual, the class with the highest probability (0.72) was the 'minor risk of mortality'. Therefore, the model predicted that this specific patient would have a 'minor risk of mortality'. This prediction matched the actual records, so in this instance the model predicted correctly.
+            You can tell that the model arrived at this correct prediction because the indivudual was young, paid with Medicaid, was a medical admittance and did not undergo a procedure (CCS Procedure 0). Some features that lowered the probability of it being a 'minor risk' were; that it was an emergency department admittance and that it was a digestive system diagnosis (APR DRG 254).
             
             """
         ),
